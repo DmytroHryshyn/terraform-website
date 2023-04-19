a0671f28622acd33ba6f46605e7ae7b034b4d7cc
dhryshyn <dev.dmytroh@gmail.com>
2023-04-19 13:01:11 +0300

    Test commit

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,14 +1,12 @@
-import NotFound from './404'
-import Bugsnag from '@hashicorp/platform-runtime-error-monitoring'
-
+/*This file should be deleted. Please migrate its contents to appropriate files*/
+import NotFound from './404';
+import Bugsnag from '@hashicorp/platform-runtime-error-monitoring';
 function Error({ statusCode }) {
-  return <NotFound />
+	return <NotFound />;
 }
-
 Error.getInitialProps = ({ res, err }) => {
-  if (err) Bugsnag.notify(err)
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
-
-export default Error
+	if (err) Bugsnag.notify(err);
+	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+	return { statusCode };
+};
+export default Error;
