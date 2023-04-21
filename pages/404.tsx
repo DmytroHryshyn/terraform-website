847aa6a4d7912325df15d7afc3b746c79278b2a2
dhryshyn <dev.dmytroh@gmail.com>
2023-04-21 17:13:05 +0300

    Migrated next/13/new-link

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,21 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import { useErrorPageAnalytics } from '@hashicorp/platform-analytics';
-
 export default function NotFound() {
-  useErrorPageAnalytics(404);
-
-  return (
-    <div id="p-404" className="g-grid-container">
-      <h1 className="g-type-display-1">Page Not Found</h1>
-      <p>
-        We&lsquo;re sorry but we can&lsquo;t find the page you&lsquo;re looking
-        for.
-      </p>
-      <p>
-        <Link href="/">
-          <a>Back to Home</a>
-        </Link>
-      </p>
-    </div>
-  )
+	useErrorPageAnalytics(404);
+	return (
+		<div id="p-404" className="g-grid-container">
+			<h1 className="g-type-display-1">Page Not Found</h1>
+			<p>
+				We&lsquo;re sorry but we can&lsquo;t find the page you&lsquo;re
+				looking for.
+			</p>
+			<p>
+				<Link href="/">Back to Home</Link>
+			</p>
+		</div>
+	);
 }
