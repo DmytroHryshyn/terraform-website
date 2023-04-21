5f96adadccb17c62d97cdbe91831f1d1a27c9126
dhryshyn <dev.dmytroh@gmail.com>
2023-04-21 16:52:22 +0300

    Test123

--- pages/enterprise/[[...page]].tsx
+++ pages/enterprise/[[...page]].tsx
@@ -1,80 +1,72 @@
-import { productName, productSlug } from 'data/metadata'
-import DocsPage from 'components/docs-page'
-import otherDocsData from 'data/other-docs-nav-data.json'
-
+import { productName } from 'data/metadata';
+import DocsPage from 'components/docs-page';
+import otherDocsData from 'data/other-docs-nav-data.json';
 // Imports below are only used server-side
-import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
-import path from 'path'
-import { rehypePlugins, remarkPlugins } from 'lib/remark-rehype-plugins'
-import { remarkRewriteAssets } from 'lib/remark-rewrite-assets'
-import { remarkTfeContentExclusion } from 'lib/remark-tfe-content-exclusion'
-
+import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server';
+import path from 'path';
+import { rehypePlugins, remarkPlugins } from 'lib/remark-rehype-plugins';
+import { remarkRewriteAssets } from 'lib/remark-rewrite-assets';
+import { remarkTfeContentExclusion } from 'lib/remark-tfe-content-exclusion';
 //  Configure the docs path
-const BASE_ROUTE = 'enterprise'
+const BASE_ROUTE = 'enterprise';
 const NAV_DATA = path.join(
-  process.env.NAV_DATA_DIRNAME,
-  BASE_ROUTE + '-nav-data.json'
-)
-const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE)
-const PRODUCT = { name: productName, slug: 'terraform' } as const
-const PROJECT_NAME = 'Terraform Enterprise'
-
-const SOURCE_REPO = 'ptfe-releases'
+	process.env.NAV_DATA_DIRNAME,
+	BASE_ROUTE + '-nav-data.json',
+);
+const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE);
+const PRODUCT = { name: productName, slug: 'terraform' } as const;
+const PROJECT_NAME = 'Terraform Enterprise';
+const SOURCE_REPO = 'ptfe-releases';
 // const DEFAULT_BRANCH = 'main'
-
 export default function EnterpriseLayout(props) {
-  // add the "other docs" section to the bottom of the nav data
-  const modifiedProps = Object.assign({}, props)
-  modifiedProps.navData = modifiedProps.navData.concat(otherDocsData)
-
-  return (
-    <DocsPage
-      baseRoute={BASE_ROUTE}
-      product={PRODUCT}
-      staticProps={modifiedProps}
-      projectName={PROJECT_NAME}
-      showEditPage={false}
-      showVersionSelect
-    />
-  )
+	// add the "other docs" section to the bottom of the nav data
+	const modifiedProps = Object.assign({}, props);
+	modifiedProps.navData = modifiedProps.navData.concat(otherDocsData);
+	return (
+		<DocsPage
+			baseRoute={BASE_ROUTE}
+			product={PRODUCT}
+			staticProps={modifiedProps}
+			projectName={PROJECT_NAME}
+			showEditPage={false}
+			showVersionSelect
+		/>
+	);
 }
-
-
 const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
-  process.env.IS_CONTENT_PREVIEW &&
-    process.env.PREVIEW_FROM_REPO === SOURCE_REPO
-    ? {
-        strategy: 'fs',
-        localContentDir: CONTENT_DIR,
-        navDataFile: NAV_DATA,
-        product: SOURCE_REPO,
-        githubFileUrl(filepath) {
-          // enterprise pages will not be editable by contributors.
-          //
-          // todo: maybe link to a GitHub issue?
-          return null
-        },
-        remarkPlugins: (params, version) => [
-          ...remarkPlugins,
-          remarkRewriteAssets({
-            product: SOURCE_REPO,
-            version: process.env.CURRENT_GIT_BRANCH,
-            getAssetPathParts: (nodeUrl) => ['website', nodeUrl],
-          }),
-          [remarkTfeContentExclusion, { version }],
-        ],
-        rehypePlugins,
-      }
-    : {
-        fallback: 'blocking',
-        revalidate: 3600, // 1 hour
-        strategy: 'remote',
-        basePath: BASE_ROUTE,
-        product: SOURCE_REPO,
-        remarkPlugins,
-        rehypePlugins,
-        enabledVersionedDocs: true,
-      }
-)
-
-export { getStaticPaths, getStaticProps }
+	process.env.IS_CONTENT_PREVIEW &&
+		process.env.PREVIEW_FROM_REPO === SOURCE_REPO
+		? {
+				strategy: 'fs',
+				localContentDir: CONTENT_DIR,
+				navDataFile: NAV_DATA,
+				product: SOURCE_REPO,
+				githubFileUrl(filepath) {
+					// enterprise pages will not be editable by contributors.
+					//
+					// todo: maybe link to a GitHub issue?
+					return null;
+				},
+				remarkPlugins: (params, version) => [
+					...remarkPlugins,
+					remarkRewriteAssets({
+						product: SOURCE_REPO,
+						version: process.env.CURRENT_GIT_BRANCH,
+						getAssetPathParts: (nodeUrl) => ['website', nodeUrl],
+					}),
+					[remarkTfeContentExclusion, { version }],
+				],
+				rehypePlugins,
+		  }
+		: {
+				fallback: 'blocking',
+				revalidate: 3600,
+				strategy: 'remote',
+				basePath: BASE_ROUTE,
+				product: SOURCE_REPO,
+				remarkPlugins,
+				rehypePlugins,
+				enabledVersionedDocs: true,
+		  },
+);
+export { getStaticPaths, getStaticProps };
--- pages/plugin/log/[[...page]].tsx
+++ pages/plugin/log/[[...page]].tsx
@@ -1,82 +1,75 @@
-import { productName, productSlug } from 'data/metadata'
-import DocsPage from 'components/docs-page'
-import ProviderTable from 'components/provider-table'
-import otherPluginsData from 'data/other-plugins-nav-data.json'
-
+import { productName } from 'data/metadata';
+import DocsPage from 'components/docs-page';
+import ProviderTable from 'components/provider-table';
+import otherPluginsData from 'data/other-plugins-nav-data.json';
 // Imports below are only used server-side
-import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
-import path from 'path'
-import { rehypePlugins, remarkPlugins } from 'lib/remark-rehype-plugins'
-import { remarkRewriteAssets } from 'lib/remark-rewrite-assets'
-
+import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server';
+import path from 'path';
+import { rehypePlugins, remarkPlugins } from 'lib/remark-rehype-plugins';
+import { remarkRewriteAssets } from 'lib/remark-rewrite-assets';
 //  Configure the docs path
-const BASE_ROUTE = 'plugin/log'
-const NAV_DATA_PREFIX = 'plugin-log'
+const BASE_ROUTE = 'plugin/log';
+const NAV_DATA_PREFIX = 'plugin-log';
 const NAV_DATA = path.join(
-  process.env.NAV_DATA_DIRNAME,
-  NAV_DATA_PREFIX + '-nav-data.json'
-)
-
-const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE)
-const PRODUCT = { name: productName, slug: 'terraform' } as const
-
-const SOURCE_REPO = 'terraform-plugin-log'
-const PROJECT_NAME = 'Plugin Log'
-const DEFAULT_BRANCH = 'main'
-
+	process.env.NAV_DATA_DIRNAME,
+	NAV_DATA_PREFIX + '-nav-data.json',
+);
+const CONTENT_DIR = path.join(process.env.CONTENT_DIRNAME, BASE_ROUTE);
+const PRODUCT = { name: productName, slug: 'terraform' } as const;
+const SOURCE_REPO = 'terraform-plugin-log';
+const PROJECT_NAME = 'Plugin Log';
+const DEFAULT_BRANCH = 'main';
 export default function PluginLogLayout(props) {
-  // display "Other Plugin Docs" section
-  const modifiedProps = Object.assign({}, props)
-  // filter out the link for this page
-  modifiedProps.navData = modifiedProps.navData.concat(
-    otherPluginsData.filter(({ href }) => !href?.includes(BASE_ROUTE))
-  )
-  return (
-    <>
-      <DocsPage
-        additionalComponents={{ ProviderTable }}
-        baseRoute={BASE_ROUTE}
-        product={PRODUCT}
-        projectName={PROJECT_NAME}
-        staticProps={modifiedProps}
-        showVersionSelect
-      />
-    </>
-  )
+	// display "Other Plugin Docs" section
+	const modifiedProps = Object.assign({}, props);
+	// filter out the link for this page
+	modifiedProps.navData = modifiedProps.navData.concat(
+		otherPluginsData.filter(({ href }) => !href?.includes(BASE_ROUTE)),
+	);
+	return (
+		<>
+			<DocsPage
+				additionalComponents={{ ProviderTable }}
+				baseRoute={BASE_ROUTE}
+				product={PRODUCT}
+				projectName={PROJECT_NAME}
+				staticProps={modifiedProps}
+				showVersionSelect
+			/>
+		</>
+	);
 }
-
 const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
-  process.env.IS_CONTENT_PREVIEW &&
-    process.env.PREVIEW_FROM_REPO === SOURCE_REPO
-    ? {
-        strategy: 'fs',
-        localContentDir: CONTENT_DIR,
-        navDataFile: NAV_DATA,
-        product: SOURCE_REPO,
-        githubFileUrl(filepath) {
-          return `https://github.com/hashicorp/${SOURCE_REPO}/blob/${DEFAULT_BRANCH}/${filepath}`
-        },
-        remarkPlugins: (params) => [
-          ...remarkPlugins,
-          remarkRewriteAssets({
-            product: SOURCE_REPO,
-            version: process.env.CURRENT_GIT_BRANCH,
-            getAssetPathParts: (nodeUrl) => ['website', nodeUrl],
-          }),
-        ],
-        rehypePlugins,
-      }
-    : {
-        fallback: 'blocking',
-        revalidate: 3600, // 1 hour
-        strategy: 'remote',
-        basePath: BASE_ROUTE,
-        navDataPrefix: NAV_DATA_PREFIX,
-        product: SOURCE_REPO,
-        remarkPlugins,
-        rehypePlugins,
-        enabledVersionedDocs: true,
-      }
-)
-
-export { getStaticPaths, getStaticProps }
+	process.env.IS_CONTENT_PREVIEW &&
+		process.env.PREVIEW_FROM_REPO === SOURCE_REPO
+		? {
+				strategy: 'fs',
+				localContentDir: CONTENT_DIR,
+				navDataFile: NAV_DATA,
+				product: SOURCE_REPO,
+				githubFileUrl(filepath) {
+					return `https://github.com/hashicorp/${SOURCE_REPO}/blob/${DEFAULT_BRANCH}/${filepath}`;
+				},
+				remarkPlugins: (params) => [
+					...remarkPlugins,
+					remarkRewriteAssets({
+						product: SOURCE_REPO,
+						version: process.env.CURRENT_GIT_BRANCH,
+						getAssetPathParts: (nodeUrl) => ['website', nodeUrl],
+					}),
+				],
+				rehypePlugins,
+		  }
+		: {
+				fallback: 'blocking',
+				revalidate: 3600,
+				strategy: 'remote',
+				basePath: BASE_ROUTE,
+				navDataPrefix: NAV_DATA_PREFIX,
+				product: SOURCE_REPO,
+				remarkPlugins,
+				rehypePlugins,
+				enabledVersionedDocs: true,
+		  },
+);
+export { getStaticPaths, getStaticProps };
