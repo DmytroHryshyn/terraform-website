9ca6f8f9b81575d7a75131eb4699ef958fb6ad71
dhryshyn <dev.dmytroh@gmail.com>
2023-04-30 11:27:48 +0300

    Migrations: stagedJobsUniqueName

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,39 +1,28 @@
 import s from './style.module.css'
 import Link from 'next/link'
-
 export default function Footer({ openConsentManager }) {
   return (
     <footer className={s.footer}>
       <div className="g-grid-container">
         <ul className={`${s.footerLinks} ${s.nav}`}>
           <li>
-            <Link href="/">
-              <a>Overview</a>
-            </Link>
+            <Link href="/">Overview</Link>
           </li>
           <li>
             {' '}
-            <Link href="/docs">
-              <a>Docs</a>
-            </Link>
+            <Link href="/docs">Docs</Link>
           </li>
           <li>
-            <Link href="/plugin">
-              <a>Extend</a>
-            </Link>
+            <Link href="/plugin">Extend</Link>
           </li>
           <li>
             <a href="https://www.hashicorp.com/privacy">Privacy</a>
           </li>
           <li>
-            <Link href="/security">
-              <a>Security</a>
-            </Link>
+            <Link href="/security">Security</Link>
           </li>
           <li>
-            <Link href="/assets/files/press-kit.zip">
-              <a>Press Kit</a>
-            </Link>
+            <Link href="/assets/files/press-kit.zip">Press Kit</Link>
           </li>
           <li>
             <a onClick={openConsentManager}>Consent Manager</a>
